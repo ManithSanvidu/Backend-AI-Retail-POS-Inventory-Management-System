c243ac3aecccc032b1ce4a879aa75cb341184647
= <=>
2026-06-02 20:35:03 +0530

    fix errors

--- src/server.js
+++ src/server.js
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
-
-const branchRoutes = require("./routes/branchRoutes.js");
-
-const app = express();
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Routes
-app.use("/api/branches", branchRoutes);
-
-// Test Route
-app.get("/", (req, res) => {
-  res.json({
-    message: "AI Retail POS Backend Running",
-  });
-});
-
-// Database Connection
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB Connected");
-
-    const PORT = process.env.PORT || 5000;
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-=======
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -51,4 +11,3 @@ connectDB();
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
->>>>>>> 4fe9a7dd5ee50f1628986f227ff4046b380ef6a7
